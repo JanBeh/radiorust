cc78950286c6375c09faad4f0b0cc1b4c3014b76
Jan Behrens <jbe@magnetkern.de>
2022-09-19 14:08:24 +0200

    Use features for SoapySDR and cpal support
    Dependencies on `soapysdr` and `cpal` have been made optional and
    metadata for `docs.rs` has been added to not use these features.

--- src/blocks/io/audio.rs
+++ src/blocks/io/audio.rs
@@ -1,110 +1,12 @@
 //! Blocks accessing audio interfaces
+//!
+//! Use feature "`cpal`" for audio interface support.
 
-use crate::blocks::Samples;
-use crate::bufferpool::*;
-use crate::flow::*;
+#[cfg(feature = "cpal")]
+mod cpal;
 
-use cpal::traits::{DeviceTrait as _, HostTrait as _, StreamTrait as _};
-use num::Complex;
-
-/// Audio player
-pub struct Audio {
-    receiver: Receiver<Samples<Complex<f32>>>,
-    stream: cpal::Stream,
-}
-
-impl Consumer<Samples<Complex<f32>>> for Audio {
-    fn receiver(&self) -> &Receiver<Samples<Complex<f32>>> {
-        &self.receiver
-    }
-}
-
-impl Audio {
-    /// Create `Audio` block for playback only with given `sample_rate` and
-    /// desired `buffer_size`
-    pub fn for_playback(sample_rate: f64, buffer_size: usize) -> Self {
-        let rt = tokio::runtime::Handle::current();
-        let sample_rate_int = sample_rate.round() as u32;
-        let mut buffer_size: u32 = buffer_size.try_into().unwrap();
-        let receiver = Receiver::<Samples<Complex<f32>>>::new();
-        let mut input = receiver.stream();
-        let host = cpal::default_host();
-        let device = host
-            .default_output_device()
-            .expect("no output device available");
-        let supported_ranges = device
-            .supported_output_configs()
-            .expect("no supported audio config");
-        let range = supported_ranges
-            .filter(|range| {
-                range.channels() == 1
-                    && range.min_sample_rate().0 <= sample_rate_int
-                    && range.max_sample_rate().0 >= sample_rate_int
-                    && range.sample_format() == cpal::SampleFormat::F32
-            })
-            .next()
-            .expect("no suitable audio config found");
-        let supported_config = range.with_sample_rate(cpal::SampleRate(sample_rate_int));
-        match supported_config.buffer_size() {
-            &cpal::SupportedBufferSize::Range { min, max } => {
-                buffer_size = buffer_size.min(max).max(min)
-            }
-            &cpal::SupportedBufferSize::Unknown => (),
-        }
-        let config = cpal::StreamConfig {
-            channels: 1,
-            sample_rate: cpal::SampleRate(sample_rate_int),
-            buffer_size: cpal::BufferSize::Fixed(buffer_size),
-        };
-        let err_fn = |err| eprintln!("an error occurred on the output audio stream: {}", err);
-        let mut current_chunk_opt: Option<Chunk<Complex<f32>>> = None;
-        let mut current_pos: usize = 0;
-        let write_audio = move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
-            for sample in data.iter_mut() {
-                let current_chunk = if current_chunk_opt.is_some() {
-                    current_chunk_opt.take().unwrap()
-                } else {
-                    loop {
-                        match rt.block_on(input.recv_lowlat(0)) {
-                            Ok(Samples {
-                                sample_rate: rcvd_sample_rate,
-                                chunk,
-                            }) => {
-                                assert_eq!(
-                                    rcvd_sample_rate, sample_rate,
-                                    "audio block received samples with unexpected sample rate"
-                                );
-                                break chunk;
-                            }
-                            Err(_) => (),
-                        }
-                    }
-                };
-                *sample = current_chunk[current_pos].re;
-                current_pos += 1;
-                if current_pos < current_chunk.len() {
-                    current_chunk_opt = Some(current_chunk)
-                } else {
-                    current_pos = 0;
-                    current_chunk_opt = None;
-                }
-            }
-        };
-        let stream = device
-            .build_output_stream(&config, write_audio, err_fn)
-            .unwrap();
-        stream.play().unwrap();
-        Self { receiver, stream }
-    }
-    /// Resume playback
-    pub fn resume(&self) {
-        self.stream.play().unwrap();
-    }
-    /// Pause playback
-    pub fn pause(&self) {
-        self.stream.pause().unwrap();
-    }
-}
+#[cfg(feature = "cpal")]
+pub use self::cpal::*;
 
 #[cfg(test)]
 mod tests {}
--- src/blocks/io/rf.rs
+++ src/blocks/io/rf.rs
@@ -1,53 +1,12 @@
 //! Blocks interfacing RF hardware
+//!
+//! Use feature "`soapy`" for SoapySDR support.
 
-use crate::blocks::Samples;
-use crate::bufferpool::*;
-use crate::flow::*;
+#[cfg(feature = "soapy")]
+mod soapy;
 
-use num::Complex;
-use soapysdr::{self, Direction::Rx};
-use tokio::{runtime, task::spawn_blocking};
-
-/// Block for receiving from an SDR device
-pub struct Sdr {
-    sender: Sender<Samples<Complex<f32>>>,
-}
-
-impl Producer<Samples<Complex<f32>>> for Sdr {
-    fn connector(&self) -> SenderConnector<Samples<Complex<f32>>> {
-        self.sender.connector()
-    }
-}
-
-impl Sdr {
-    /// Create new `Sdr` block with given center `frequency`
-    pub fn for_receiving(sample_rate: f64, frequency: f64, bandwidth: f64) -> Self {
-        let sender = Sender::new();
-        let output = sender.clone();
-        let dev = soapysdr::Device::new("").unwrap();
-        dev.set_frequency(Rx, 0, frequency, "").unwrap();
-        dev.set_sample_rate(Rx, 0, sample_rate).unwrap();
-        dev.set_bandwidth(Rx, 0, bandwidth).unwrap();
-        let mut rx = dev.rx_stream::<Complex<f32>>(&[0]).unwrap();
-        let mtu = rx.mtu().unwrap();
-        rx.activate(None).unwrap();
-        spawn_blocking(move || {
-            let rt = runtime::Handle::current();
-            let mut buf_pool = ChunkBufPool::<Complex<f32>>::new();
-            loop {
-                let mut buffer = buf_pool.get();
-                buffer.resize_with(mtu, Default::default);
-                let count = rx.read(&[&mut buffer], 1000000).unwrap();
-                buffer.truncate(count);
-                rt.block_on(output.send(Samples {
-                    sample_rate,
-                    chunk: buffer.finalize(),
-                }));
-            }
-        });
-        Sdr { sender }
-    }
-}
+#[cfg(feature = "soapy")]
+pub use soapy::*;
 
 #[cfg(test)]
 mod tests {}

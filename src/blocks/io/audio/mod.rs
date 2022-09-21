7865dd301b335b263861cc95cadd4e41259137d3
Jan Behrens <jbe@magnetkern.de>
2022-09-21 19:34:10 +0200

    Moved unstable API notice from io to audio mod
    The API is overall unstable yet, but the `blocks::io::audio` module is
    expected to change the most. The warning about the unstable API has been
    moved from `blocks::io` to `blocks::io::audio`.

--- src/blocks/io/audio/mod.rs
+++ src/blocks/io/audio/mod.rs
@@ -1,6 +1,8 @@
 //! Blocks accessing audio interfaces
 //!
 //! Use feature "`cpal`" for audio interface support.
+//!
+//! **Note: The API of this module is highly unstable yet and subject to change.**
 
 #[cfg(feature = "cpal")]
 mod cpal;
--- src/blocks/io/mod.rs
+++ src/blocks/io/mod.rs
@@ -1,7 +1,5 @@
 //! External sources and sinks
 //!
-//! Note: The API of this module is highly unstable yet and subject to change.
-//!
 //! The [`audio`] and [`rf`] modules contain blocks that allow accessing
 //! hardware audio or radio interfaces. The [`raw_in`] and [`raw_out`] modules
 //! allow reading or writing I/Q data as bytes (e.g. from/to files).

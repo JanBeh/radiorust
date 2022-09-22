f6d320389b16ae7b162669e6fe58f887f0fc77eb
Jan Behrens <jbe@magnetkern.de>
2022-09-22 20:33:29 +0200

    Added FM modulator
    `blocks::modulation::FmMod` has been added.

--- src/blocks/modulation.rs
+++ src/blocks/modulation.rs
@@ -9,6 +9,98 @@ use crate::samples::Samples;
 use tokio::sync::watch;
 use tokio::task::spawn;
 
+/// FM modulator block
+pub struct FmMod<Flt> {
+    receiver: Receiver<Samples<Complex<Flt>>>,
+    sender: Sender<Samples<Complex<Flt>>>,
+    deviation: watch::Sender<f64>,
+}
+
+impl<Flt> Consumer<Samples<Complex<Flt>>> for FmMod<Flt>
+where
+    Flt: Clone,
+{
+    fn receiver(&self) -> &Receiver<Samples<Complex<Flt>>> {
+        &self.receiver
+    }
+}
+
+impl<Flt> Producer<Samples<Complex<Flt>>> for FmMod<Flt>
+where
+    Flt: Clone,
+{
+    fn connector(&self) -> SenderConnector<Samples<Complex<Flt>>> {
+        self.sender.connector()
+    }
+}
+
+impl<Flt> FmMod<Flt>
+where
+    Flt: Float,
+{
+    /// Create new FM modulator with given frequency deviation in hertz
+    pub fn new(mut deviation: f64) -> Self {
+        use std::f64::consts::TAU;
+        let receiver = Receiver::<Samples<Complex<Flt>>>::new();
+        let sender = Sender::<Samples<Complex<Flt>>>::new();
+        let (deviation_send, mut deviation_recv) = watch::channel(deviation);
+        let mut input = receiver.stream();
+        let output = sender.clone();
+        let mut buf_pool = ChunkBufPool::<Complex<Flt>>::new();
+        let mut current_phase: Flt = Flt::zero();
+        spawn(async move {
+            loop {
+                match input.recv().await {
+                    Ok(Samples {
+                        sample_rate,
+                        chunk: input_chunk,
+                    }) => {
+                        match deviation_recv.has_changed() {
+                            Ok(false) => (),
+                            Ok(true) => deviation = deviation_recv.borrow_and_update().clone(),
+                            Err(_) => return,
+                        }
+                        let factor: Flt = flt!(deviation / sample_rate * TAU);
+                        let mut output_chunk = buf_pool.get_with_capacity(input_chunk.len());
+                        for &sample in input_chunk.iter() {
+                            current_phase += sample.re * factor;
+                            current_phase %= Flt::TAU();
+                            let (im, re) = current_phase.sin_cos();
+                            output_chunk.push(Complex::<Flt>::new(re, im));
+                        }
+                        output
+                            .send(Samples {
+                                sample_rate,
+                                chunk: output_chunk.finalize(),
+                            })
+                            .await;
+                    }
+                    Err(err) => {
+                        output.forward_error(err).await;
+                        if err == RecvError::Closed {
+                            return;
+                        }
+                    }
+                }
+            }
+        });
+        Self {
+            receiver,
+            sender,
+            deviation: deviation_send,
+        }
+    }
+    /// Get frequency deviation in hertz
+    pub fn deviation(&self) -> f64 {
+        self.deviation.borrow().clone()
+    }
+    /// Set frequency deviation in hertz
+    pub fn set_deviation(&self, deviation: f64) -> &Self {
+        self.deviation.send_replace(deviation);
+        self
+    }
+}
+
 /// FM demodulator block
 pub struct FmDemod<Flt> {
     receiver: Receiver<Samples<Complex<Flt>>>,

9065410ec3d0d9456720c9cc963ebca74654cd2c
Jan Behrens <jbe@magnetkern.de>
2022-10-20 17:07:39 +0200

    Sliding window for bandwidth_meter example
    Example `bandwidth_meter` has been improved to use the
    `radiorust::blocks::Overlapper` block and a
    `radiorust::windowing::Kaiser` window.

--- examples/bandwidth_meter/main.rs
+++ examples/bandwidth_meter/main.rs
@@ -2,6 +2,7 @@ use radiorust::blocks;
 use radiorust::flow::*;
 use radiorust::metering;
 use radiorust::samples::*;
+use radiorust::windowing;
 
 use num::Complex;
 use soapysdr::Direction::Rx;
@@ -26,15 +27,20 @@ struct Args {
     /// Maximum bandwidth in kHz
     #[arg(short = 'm', long, default_value = "60")]
     max_bandwidth: f64,
+    /// Quality
+    #[arg(short = 'q', long, default_value = "4")]
+    quality: u16,
 }
 
 #[tokio::main]
 async fn main() {
     let args = Args::parse();
     assert!(args.max_bandwidth > 0.0 && args.max_bandwidth <= 80.0);
+    assert!(args.quality > 0);
     let frequency = args.frequency * 1e6;
     let freq_offset = args.freq_offset * 1e3;
     let max_bandwidth = args.max_bandwidth * 1e3;
+    let quality: usize = args.quality.try_into().unwrap();
     let hw_frequency = frequency + freq_offset;
     let sample_rate = 1024000.0;
     let bandwidth = 1024000.0;
@@ -49,7 +55,7 @@ async fn main() {
     let freq_shifter = blocks::FreqShifter::<f32>::with_shift(freq_offset);
     println!("Frequency: {}", hw_frequency - freq_offset);
     freq_shifter.connect_to_producer(&sdr_rx);
-    let downsampler = blocks::Downsampler::<f32>::new(2048, 102400.0, max_bandwidth);
+    let downsampler = blocks::Downsampler::<f32>::new(1024, 102400.0, max_bandwidth);
     downsampler.connect_to_producer(&freq_shifter);
     let filter = blocks::filters::Filter::new(move |_, freq| {
         if freq.abs() <= max_bandwidth / 2.0 {
@@ -59,8 +65,10 @@ async fn main() {
         }
     });
     filter.connect_to_producer(&downsampler);
-    let fourier = blocks::Fourier::new();
-    fourier.connect_to_producer(&filter);
+    let overlapper = blocks::Overlapper::new(quality);
+    overlapper.connect_to_producer(&filter);
+    let fourier = blocks::Fourier::with_window(windowing::Kaiser::with_null_at_bin(quality as f64));
+    fourier.connect_to_producer(&overlapper);
     let receiver = Receiver::<Samples<Complex<f32>>>::new();
     receiver.connect_to_producer(&fourier);
     let mut output = receiver.stream();
@@ -70,7 +78,7 @@ async fn main() {
         match output.recv().await {
             Ok(samples) => {
                 let bw = metering::bandwidth(0.01, &samples);
-                if values.len() >= 50 {
+                if values.len() >= 100 {
                     values.pop_front();
                 }
                 values.push_back(bw);
@@ -81,7 +89,7 @@ async fn main() {
                     }
                 }
                 i += 1;
-                if i >= 25 {
+                if i >= 50 {
                     println!("Max bandwidth within 1 second {max_bw}");
                     i = 0;
                 }

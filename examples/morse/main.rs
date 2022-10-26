bb28ea12663e290b4417228f159d82b5f5ea4b69
Jan Behrens <jbe@magnetkern.de>
2022-10-26 02:44:48 +0200

    Use SlewRateLimiter in morse example
    Use `crate::blocks::filters::SlewRateLimiter` in `examples/morse`. Now
    the example also filters the signal before (and not after) shifting it
    to 700 Hz.

--- examples/morse/main.rs
+++ examples/morse/main.rs
@@ -4,20 +4,22 @@ use radiorust::prelude::*;
 async fn main() {
     let keyer =
         blocks::morse::Keyer::new(4096, 48000.0, blocks::morse::Speed::from_paris_wpm(16.0));
-    let volume = blocks::Function::with_closure(|x| 0.5 * x);
-    volume.connect_to_producer(&keyer);
-    let audio_mod = blocks::FreqShifter::with_shift(700.0);
-    audio_mod.connect_to_producer(&volume);
+    let limiter = blocks::filters::SlewRateLimiter::new(100.0);
+    limiter.connect_to_producer(&keyer);
     let filter = blocks::Filter::new(|_, freq| {
-        if freq >= 300.0 && freq <= 2700.0 {
+        if freq.abs() <= 100.0 {
             Complex::from(1.0)
         } else {
             Complex::from(0.0)
         }
     });
-    filter.connect_to_producer(&audio_mod);
+    filter.connect_to_producer(&limiter);
+    let volume = blocks::Function::with_closure(|x| 0.5 * x);
+    volume.connect_to_producer(&filter);
+    let audio_mod = blocks::FreqShifter::with_shift(700.0);
+    audio_mod.connect_to_producer(&volume);
     let playback = blocks::io::audio::cpal::AudioPlayer::new(48000.0, 2 * 4096);
-    playback.connect_to_producer(&filter);
+    playback.connect_to_producer(&audio_mod);
     let mut rl = rustyline::Editor::<()>::new().unwrap();
     loop {
         let line = rl.readline("Enter morse message> ").unwrap();

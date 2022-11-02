00aad8638d4cd17cd3f7f4f458eac3f5abbc6835
Jan Behrens <jbe@magnetkern.de>
2022-11-02 14:07:13 +0100

    Option for Fourier block to shift DC bin to center
    Two functions have been added to `crate::blocks::analysis::Fourier`:
    `new_center_dc` and `with_window_center_dc`. These functions act like
    `new` and `with_window`, respectively, but will result in the DC bin
    rotated to the center (thus that negative frequencies are left of it
    and positive frequencies are right of it).
    
    Tests have been added for all four constructor functions of the
    `Fourier` block.

--- src/blocks/analysis.rs
+++ src/blocks/analysis.rs
@@ -13,6 +13,16 @@ use tokio::task::spawn;
 use std::sync::Arc;
 
 /// Block performing a Fourier analysis
+///
+/// Note that [`Fourier::new`] and [`Fourier::with_window`] will result in the
+/// DC bin at index `0` and negative frequencies being located in the second
+/// half.
+/// To rotate the DC frequency to the center (and have negative frequencies
+/// left of it and positive frequencies right of it), use
+/// [`Fourier::new_center_dc`] or [`Fourier::with_window_center_dc`],
+/// respectively.
+/// In case of an even [`Chunk`] length `n`, the rotation will result in the
+/// DC bin being at index `n / 2`.
 pub struct Fourier<Flt> {
     receiver_connector: ReceiverConnector<Samples<Complex<Flt>>>,
     sender_connector: SenderConnector<Samples<Complex<Flt>>>,
@@ -36,10 +46,27 @@ where
 {
     /// Create `Fourier` block without windowing
     pub fn new() -> Self {
-        Self::with_window(windowing::Rectangular)
+        Self::new_internal(windowing::Rectangular, false)
+    }
+    /// Create `Fourier` block without windowing but rotating DC to center
+    pub fn new_center_dc() -> Self {
+        Self::new_internal(windowing::Rectangular, true)
     }
     /// Create `Fourier` block with windowing
     pub fn with_window<W>(window: W) -> Self
+    where
+        W: Window + Send + 'static,
+    {
+        Self::new_internal(window, false)
+    }
+    /// Create `Fourier` block with windowing and rotating DC to center
+    pub fn with_window_center_dc<W>(window: W) -> Self
+    where
+        W: Window + Send + 'static,
+    {
+        Self::new_internal(window, true)
+    }
+    fn new_internal<W>(window: W, center_dc: bool) -> Self
     where
         W: Window + Send + 'static,
     {
@@ -83,6 +110,9 @@ where
                             output_chunk[idx] *= window_values[idx];
                         }
                         fft.as_ref().unwrap().process(&mut output_chunk);
+                        if center_dc {
+                            output_chunk.rotate_right(n / 2);
+                        }
                         if let Err(_) = sender
                             .send(Samples {
                                 sample_rate,
@@ -112,4 +142,74 @@ where
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use super::*;
+    const PRECISION: f64 = 1e-10;
+    pub fn assert_approx(a: f64, b: f64) {
+        if !((a - b).abs() <= PRECISION || (a / b).ln().abs() <= PRECISION) {
+            panic!("{a} and {b} are not approximately equal");
+        }
+    }
+    #[tokio::test]
+    async fn test_fourier() {
+        let (sender, sender_connector) = new_sender();
+        let fourier1 = Fourier::<f64>::new();
+        let fourier2 = Fourier::<f64>::new_center_dc();
+        let (mut receiver1, receiver1_connector) = new_receiver();
+        let (mut receiver2, receiver2_connector) = new_receiver();
+        fourier1.feed_from(&sender_connector);
+        fourier2.feed_from(&sender_connector);
+        fourier1.feed_into(&receiver1_connector);
+        fourier2.feed_into(&receiver2_connector);
+        let mut buf_pool = ChunkBufPool::new();
+        let mut chunk_buf = buf_pool.get();
+        chunk_buf.push(Complex::new(1.0, 0.0));
+        chunk_buf.push(Complex::new(1.0, 0.0));
+        chunk_buf.push(Complex::new(1.0, 0.0));
+        sender.send(Samples {
+            sample_rate: 48000.0,
+            chunk: chunk_buf.finalize(),
+        }).await.unwrap();
+        let output1 = receiver1.recv().await.unwrap();
+        let output2 = receiver2.recv().await.unwrap();
+        assert_approx(output1.chunk[0].re, 3.0);
+        assert_approx(output1.chunk[0].im, 0.0);
+        assert_approx(output1.chunk[1].re, 0.0);
+        assert_approx(output1.chunk[1].im, 0.0);
+        assert_approx(output1.chunk[2].re, 0.0);
+        assert_approx(output1.chunk[2].im, 0.0);
+        assert_approx(output2.chunk[0].re, 0.0);
+        assert_approx(output2.chunk[0].im, 0.0);
+        assert_approx(output2.chunk[1].re, 3.0);
+        assert_approx(output2.chunk[1].im, 0.0);
+        assert_approx(output2.chunk[2].re, 0.0);
+        assert_approx(output2.chunk[2].im, 0.0);
+        let mut chunk_buf = buf_pool.get();
+        chunk_buf.push(Complex::new(1.0, 0.0));
+        chunk_buf.push(Complex::new(1.5, 0.0));
+        chunk_buf.push(Complex::new(1.0, 0.0));
+        chunk_buf.push(Complex::new(0.5, 0.0));
+        sender.send(Samples {
+            sample_rate: 48000.0,
+            chunk: chunk_buf.finalize(),
+        }).await.unwrap();
+        let output1 = receiver1.recv().await.unwrap();
+        let output2 = receiver2.recv().await.unwrap();
+        assert_approx(output1.chunk[0].re, 4.0);
+        assert_approx(output1.chunk[0].im, 0.0);
+        assert_approx(output1.chunk[1].re, 0.0);
+        assert_approx(output1.chunk[1].im, -1.0);
+        assert_approx(output1.chunk[2].re, 0.0);
+        assert_approx(output1.chunk[2].im, 0.0);
+        assert_approx(output1.chunk[3].re, 0.0);
+        assert_approx(output1.chunk[3].im, 1.0);
+        assert_approx(output2.chunk[0].re, 0.0);
+        assert_approx(output2.chunk[0].im, 0.0);
+        assert_approx(output2.chunk[1].re, 0.0);
+        assert_approx(output2.chunk[1].im, 1.0);
+        assert_approx(output2.chunk[2].re, 4.0);
+        assert_approx(output2.chunk[2].im, 0.0);
+        assert_approx(output2.chunk[3].re, 0.0);
+        assert_approx(output2.chunk[3].im, -1.0);
+    }
+}
